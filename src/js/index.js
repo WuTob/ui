a2ae7d612d740b94ea71560b54a1fcc52e3c3d0f
Todd Wu <wutob12450@outlook.com>
2018-04-14 23:56:58 +0800

    update

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import 'jquery';
+
+$(document).ready(()=>{
+    let buttons = {
+        open_button    : $(".open_button"),
+        manage_button  : $(".manage_button"),
+        password_button: $(".password_button")
+    }
+
+})
