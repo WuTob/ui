6ff5437f58c7f257c3bd8fc5c76836544e356919
Todd Wu <wutob12450@outlook.com>
2018-04-17 13:04:35 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-import {sync} from "./ui";
+import * as UI from "./ui";
 import "../css/index.css"
+import "../css/animation.css"
 
 var $ = require("jquery");
 
 $(document).ready(()=>{
+    UI.animate();
     //sync();
 });
 
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,7 +1,10 @@
 var $ = require("jquery");
 
-export function sync(){
-    $(".open_button").css("height", $(".open_button").css("width"));
-    $(".manage_button").css("height", $(".manage_button").css("width"));
-    $(".password_button").css("height", $(".password_button").css("width"));
+export function animate(){
+    $(".manage_button").addClass("bounceIn");
+
+    setTimeout(()=>{
+        $(".open_button").addClass("moveToLeft");
+        $(".password_button").addClass("moveToRight");
+    }, 1000);
 }
